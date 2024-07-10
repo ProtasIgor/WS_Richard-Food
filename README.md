52d00a23b3ed967efc6623b121cb96a7a47cbb92
ProtasIgor <21it3.protas.i@pdu.by>
2024-07-10 10:22:42 +0300

    1.0.0 - Landing Done
    Add Pictures - /img/*
    Modified styles /css/*
    Modified scripts

---
+++ README.md
@@ -0,0 +1,9 @@
+# Richard Food
+
+## Open in browser
+
+> Click to visit Website [Richard Food](https://protasigor.github.io/WS_Richard-Food)
+
+## Layout
+
+<img src='img/layout.png' alt='layout site' style="display:block;width:'100%';object-fit:contain;"></img>
--- js/script.js
+++ js/script.js
@@ -1,27 +1,45 @@
 (function () {
 
     document.addEventListener("DOMContentLoaded", () => {
-
+        // подгружаем нужные стили
         let styleBox = document.createElement('link');
         styleBox.setAttribute('rel', 'stylesheet');
 
         if (window.matchMedia('(max-width: 420px)').matches) {
-            setTimeout(()=> {
-                styleBox.setAttribute('href', 'css/style-mobile.css');
-                document.head.append(styleBox);
-            }, 0)
+            styleBox.setAttribute('href', 'css/style-mobile.css');
         }
         else {
-            setTimeout(()=> {
-                styleBox.setAttribute('href', 'css/style-desctop.css');
-                document.head.append(styleBox);
-            }, 0)
-
+            styleBox.setAttribute('href', 'css/style-desctop.css');
         }
+        document.head.append(styleBox);
     });
 
+    // свойства ScrollIntoView
+    const propertyScrollIntoView = { behavior: "smooth", block: "start", inline: "center" };
+    // ссылки в nav bar
+    const navItemsLink = document.querySelectorAll('.main__list-item');
+    // основные секции сайта
+    const sections = [
+        document.querySelector('.blok-eat'),
+        document.querySelector('.advantage'),
+        document.querySelector('.dish'),
+        document.querySelector('.download'),
+    ];
+    // добавление навигации на сайт
+    navItemsLink.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            sections[index].scrollIntoView(propertyScrollIntoView);
+        });
+    })
+    // скролл с главного фрейма
     const scrollButton = document.querySelector('.main__scroll-button');
     scrollButton.addEventListener('click', () => {
-        document.querySelector('.main').nextElementSibling.scrollIntoView({ behavior: "smooth", block: "end", inline: "center" });
+        document.querySelector('.main').nextElementSibling.scrollIntoView(propertyScrollIntoView);
     });
+    // Форма
+    document.querySelector('.footer__form-submit').addEventListener('click', (e) => {
+        e.preventDefault();
+        alert('Thank you! Your letter has been sent.');
+    });
+
 }())
